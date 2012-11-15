4c7a5335b980c6fbb1937c053652b7063f66c9bc
Matt Glazar <matt.glazar@gmail.com>
2012-11-14 18:08:17 -0800

    Error handling everywhere

--- src/io/spaceport/Swc2Swf.java
+++ src/io/spaceport/Swc2Swf.java
@@ -5,20 +5,42 @@ import java.util.zip.*;
 
 public class Swc2Swf {
 	public static final void main(String[] args) {
+		if (args.length != 2) {
+			error("Usage: java -jar swc2swf.jar input.swc output.swf");
+			return;
+		}
+
 		String inputPath = args[0];
 		String outputPath = args[1];
 
+		ZipFile file;
 		try {
-			ZipFile file = new ZipFile(inputPath);
-			ZipEntry library = file.getEntry("library.swf");
+			file = new ZipFile(inputPath);
+		} catch (IOException e) {
+			error("Error while reading SWC file: " + e.getMessage());
+			return;
+		}
+
+		ZipEntry library = file.getEntry("library.swf");
+		if (library == null) {
+			error("Missing library in SWC file; is it really a SWC file?");
+			return;
+		}
+
+		try {
 			pipe(
 				file.getInputStream(library),
 				new BufferedOutputStream(new FileOutputStream(outputPath))
 			);
 		} catch (IOException e) {
-			System.err.println("Unhandled exception:");
-			e.printStackTrace();
+			error("Error while writing SWF file: " + e.getMessage());
+			return;
 		}
+	}
+
+	private static final void error(String message) {
+		System.err.println(message);
+		System.exit(1);
 	}
 
 	// Java doesn't have basic necessities like piping streams, so we have to
