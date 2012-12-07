94554609026d5fbcfc8c10f015c5d254383a3a20
Build Server <buildserver@spaceport.io>
2012-12-06 16:54:44 -0800

    jenkins build script now places output in sdk

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -12,15 +12,21 @@ else
    echo "Performing INCREMENTAL build."
 fi
 
-#remove and recreate old staging area
+#clean old staging area
 rm -rf $STAGING_AREA/workspace/swc2swf
 mkdir $STAGING_AREA/workspace/swc2swf
+
+#clean new staging area
+rm -rf $STAGING_AREA/workspace/sdk/osx/package/lib/spaceport-support/swc2swf.jar
 
 #save change documentation to staging area
 $SP_SCRIPTS/copy_component_changes.py $WORKSPACE/../builds/$BUILD_NUMBER/changelog.xml $STAGING_AREA/changes/swc2swf.txt
 
 ant
 
-#move build artifacts to staging area
+#move build artifacts to old staging area
 cp $WORKSPACE/swc2swf.jar $STAGING_AREA/workspace/swc2swf/
+
+#move build artifacts to new staging area
+cp $WORKSPACE/swc2swf.jar $STAGING_AREA/workspace/sdk/osx/package/lib/spaceport-support/
 
